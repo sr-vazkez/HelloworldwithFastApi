00048d21fe84aca75aff24f2aa4f26cf9a17b291
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-08-19 17:49:47 -0500

    genreadno ruta items

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ app = FastAPI()
 def read_root():
     return{"Hola": "Mundo"}
 
+# Creando una nueva ruta
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 # Se usa el uvicorn main:app --reload para ejecutar esta wea
