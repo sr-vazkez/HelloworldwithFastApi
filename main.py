7a03131a6cd1ab2374ca0b152896f0195beec6e0
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-08-19 17:59:15 -0500

    modifica el archivo main.py para recibir un body del PUT request.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # Importando lo necesario
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import Optional
 # Hey mira se parece a flask
 app = FastAPI()
 # Creando rutas
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
 @app.get("/")
@@ -18,3 +25,8 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 # Se usa el uvicorn main:app --reload para ejecutar esta wea
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+         return {"item_name": item.name, "item_id": item_id}
