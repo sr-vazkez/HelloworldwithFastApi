a9f24625740d0a99d2fd0ece3aa50ab3ed4f9315
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-12-15 16:58:10 -0600

    hello world

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-# Importando lo necesario
+#Importando la clase FastAPI
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
-# Hey mira se parece a flask
+#Instanciando la clase FastAPI
 app = FastAPI()
-# Creando rutas
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-
+#path operation decoration
 @app.get("/")
-def read_root():
-    return{"Hola": "Mundo"}
-
-# Creando una nueva ruta
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-# Se usa el uvicorn main:app --reload para ejecutar esta wea
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-         return {"item_name": item.name, "item_id": item_id}
+def home():
+    return {"Hello": "World"}
