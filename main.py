c171a751e80b63b1702b10a6749c37106b54ed19
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-12-15 18:09:14 -0600

    Generando una ruta mas coqueta un modelo

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
+#importar cosas de Python
+from typing import Optional
+
+#importando pydantic
+from pydantic import BaseModel
+""" Models
+Que es un modelo?
+representa un objecto en codigo del mundo real
+pydantic es una libreria que nos permite crear modelos
+"""
 #Importando la clase FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 #Instanciando la clase FastAPI
 app = FastAPI()
 
-#path operation decoration
+#Models
+class Persona(BaseModel):
+    """ Models"""
+    nombre: str
+    apellido: str
+    edad: int
+    color_cabello: Optional[str] = None 
+    esta_casado: Optional[bool] = None     
+
+
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
-#ejecutar usando uvicorn main:app --reload
-#localhost:8000/docs para ver la documentacion 
-#localhost:8000/redoc para ver la documentacion en una pagina web
-""" Nota para entender de que va
-paths es igual a routes / endpoints
-operations es igual a methods 
-4 operations 
-get = traer 
-post =  enviar
-put = actualiza
-delete = eliminar
 
-otras operations
-options = describir opciones de comunicacion para el destino
-head = traer info de un doc pero sin el doc
-patch = modificaciones parciales
-trace = observar que le pasa a la peticion
-"""
-#path parameter obligatorios
-#Query paramether ? son opcionales 
-@app.get("/tweets/{tweet_id}")
-def tweets(tweet_id: int):
-    return {"tweet_id": tweet_id}
+#Request y Response body 
+@app.post("/persona/nueva")
+def crear_persona(persona: Persona = Body(...)):
+    return persona
